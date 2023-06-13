56f1af3b603d08dbb9eecf8d88e9aca325320fc1
hmmllnvhnn <manlanothammillan@gmail.com>
2023-06-13 13:58:34 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Sidenav from './components/Sidenav'
+import Main from './components/Main'
+import Projects from './components/Projects'
+import Contact from './components/Contact'
+import About from './components/About'
+
+function App() {
+
+  return (
+      <div>
+        <Sidenav />
+        <Main />
+        <Projects />
+        <About />
+        <Contact />
+      </div>
+  )
+}
+
+export default App
